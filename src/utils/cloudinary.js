7ea5fcad75283471eb845370ee3621432950b6e5
Abhishek <abhishek.vk396@gmail.com>
2024-06-19 02:46:12 +0530

    cloudinary setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadFile = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        fs.unlinkSync(localFilePath)
+        console.log("File has been uploaded successfully", uploadResult.url);
+        return uploadResult.url
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
